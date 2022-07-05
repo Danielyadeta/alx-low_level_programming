bb0fba0ca35d6072b60fe6051f5aca6144ddff7f
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2022-07-05 08:28:14 -0700

    Danielyadet

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 /**
- * main - Entry point
+ * main - Prints out the first 50 Fibonnaci numbers.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
+
 int main(void)
 {
+	int i;
+	long int j = 1;
+	long int k = 1;
+	long int sum;
+
+	printf("%d", 1);
+	for (i = 1; i <= 49; i++)
+	{
+		sum = j + k;
+		printf(", %ld", sum);
+		j = k;
+		k = sum;
+	}
+
+	printf("\n");
 
 	return (0);
 }
