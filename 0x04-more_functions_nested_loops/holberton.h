1cbd18ff510bfd8c78feec6309eb201bfa744111
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2022-07-07 02:14:28 -0700

    commit da

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,7 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+void _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -14,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
