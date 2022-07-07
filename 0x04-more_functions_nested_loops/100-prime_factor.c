aba5ede537c80dd8781d49ca1cdd61ce4fb60351
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2022-07-07 02:46:54 -0700

    Danielyadeta

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 /**
- * main - prints prime factors of 612852475143
- *
- * Return: Always 0 (Success)
- */
+* main - starting point of the program
+*
+* Return: Always 0
+*/
 int main(void)
 {
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
 	return (0);
 }
