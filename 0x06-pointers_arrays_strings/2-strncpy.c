4d570086fbc2b38a29a1dda01149e95c91008cd6
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2022-07-13 03:00:17 -0700

    Danielyadeta

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,24 @@
-#include "holberton.h"
-
 /**
- * _strncpy - Entry point
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
- * Return: Always 0 (Success)
- */
+* _strncpy - copies a string
+* @dest: destination string.
+* @src: source string
+* @n: number of string
+*
+* Return: char pointer
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
 	return (dest);
 }
