cf3d383204cad587e03940050cd747f168c95460
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2022-07-20 00:34:49 -0700

    Danielyadeta

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -12,4 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H *
