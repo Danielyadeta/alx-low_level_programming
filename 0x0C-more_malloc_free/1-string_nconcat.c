a13515331650794698420d81fb27d1f06cce30a3
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2022-07-28 02:59:30 -0700

    Danielyadeta

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,39 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - function that concatenates two strings.
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the second string length
+ * Return: concatenated string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *string;
+	unsigned int i = 0;
+	unsigned int j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	string = malloc(sizeof(s1) + n + 1);
+	if (string == NULL)
 		return (NULL);
-
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
+	while (s1[i] != '\0')
 	{
-		arr[co] = s2[co_2];
-		co++;
+		string[i] = s1[i];
+		i++;
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	while (j < n)
+	{
+		string[i] = s2[j];
+		i++;
+		j++;
+	}
+	string[i] = '\0';
+
+	return (string);
 }
