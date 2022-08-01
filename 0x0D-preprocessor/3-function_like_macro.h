77618680bdf0b0466cb8fa503052eb743362aabe
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2022-08-01 05:09:54 -0700

    Danielyadeta

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) (((x) < (0)) ? (-x) : (x))
-
+#endif
+#ifndef ABS_H
+#define ABS_H
+#define ABS(X) (((X) < 0) ? ((X) * -1) : ((X) * 1))
 #endif
